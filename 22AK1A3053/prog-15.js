eda90b6d84c2e9863b970cc313f04aa6c7e1dcba
pulivadhimohanapriya <mohanapuli11@gmail.com>
2024-09-14 16:01:05 +0530

    Delete 22AK1A3053 directory

--- 22AK1A3053/prog-15.js
+++
@@ -1,8 +0,0 @@
-const input = "sun rises in the east.";
-
-const words = input.split(" ");
-for (let i = 0; i < words.length; i++) {
-  if (words[i].length % 3 === 0) {
-    console.log(words[i]);
-  }
-}
